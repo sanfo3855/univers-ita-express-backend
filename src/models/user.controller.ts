08e5773ff23111b7e78e27acd3546ee7e7baebb6
Matteo Sanfelici <sanfelicimatteo@gmail.com>
2020-06-17 00:32:37 +0200

    Fix create user bug (creating only students)

--- src/models/user.controller.ts
+++ src/models/user.controller.ts
@@ -16,11 +16,17 @@ userRoutes.get('/all', exjwt({secret: jwtSecretAdmin}), async (req: express.Requ
         let items: any = await User.find({});
         console.log(items);
         items = items.map((item) => {
-            return {id: item._id, username: item.username, password: item.password, type: item.type, validity: item.validity}
+            return {
+                id: item._id,
+                username: item.username,
+                password: item.password,
+                type: item.type,
+                validity: item.validity
+            }
         });
-        resp.json({success:true, err:null, response:items});
+        resp.json({success: true, err: null, response: items});
     } catch (err) {
-        resp.json({success: false, err:"error fetching users",});
+        resp.json({success: false, err: "error fetching users",});
         console.error('Caught error', err);
     }
 });
@@ -35,10 +41,18 @@ userRoutes.post('/check', async (req: express.Request, resp: express.Response, n
                 if (user.validity === true) {
                     let token: string;
                     if (user.type === 'admin') {
-                        token = jwt.sign({id: user.id, username: user.username, type: user.type}, jwtSecretAdmin, {expiresIn: '24h'});
+                        token = jwt.sign({
+                            id: user.id,
+                            username: user.username,
+                            type: user.type
+                        }, jwtSecretAdmin, {expiresIn: '24h'});
                         resp.status(200).json({success: true, err: null, token: token})
                     } else if (user.type === 'student') {
-                        token = jwt.sign({id: user.id, username: user.username, type: user.type}, jwtSecretStudents, {expiresIn: '1m'});
+                        token = jwt.sign({
+                            id: user.id,
+                            username: user.username,
+                            type: user.type
+                        }, jwtSecretStudents, {expiresIn: '1m'});
                         resp.status(200).json({success: true, err: null, token: token})
                     } else {
                         resp.status(403).json({success: false, err: "user type wrong or not specified"})
@@ -64,39 +78,30 @@ userRoutes.post('/create', exjwt({secret: jwtSecretAdmin}), async (req: express.
         const body = req.body;
         console.log(body);
         let existUser = await User.findOne({username: body.username});
-        console.log(existUser);
+        // console.log(existUser);
         if (!existUser) {
-            let user = new User();
-            if (body.username !== 'admin') {
-                user = new User({
-                    username: body.username,
-                    password: body.password,
-                    type: body.type,
-                    validity: true
-                });
-            } else {
-                user = new User({
-                    username: body.username,
-                    password: body.password,
-                    type: body.type,
-                    validity: true
-                });
-            }
+            let user = new User({
+                username: body.username,
+                password: body.password,
+                type: body.type,
+                validity: true
+            });
             await user.save(function (err, user) {
                 if (err) return console.error(err);
                 return user;
             });
-            console.log(user.toJSON());
             resp.status(200).json({success: true, err: null});
         } else {
             resp.status(200).json({success: false, err: "Utente già esistente"});
         }
-    } catch (err) {
+    } catch
+        (err) {
         resp.status(500);
         resp.end();
         console.error('Caught error', err);
     }
-});
+})
+;
 
 userRoutes.post('/change-validity', exjwt({secret: jwtSecretAdmin}), async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
     try {
