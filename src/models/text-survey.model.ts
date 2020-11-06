5669a3b13e9fbcb3b9d8347c9692974a2db89ec2
Matteo Sanfelici <sanfelicimatteo@gmail.com>
2020-11-06 08:57:25 +0100

    text not raising error

--- src/models/text-survey.model.ts
+++ src/models/text-survey.model.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 import {Schema} from "mongoose";
 
 const TextSurveySchema = new mongoose.Schema({
-    text: {type: String, required:true},
+    text: {type: String, required:false},
     questions: {type:Schema.Types.Mixed, required: false},
     date: {type: Date, required: true}
 });
