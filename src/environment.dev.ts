1fbcb7ecfd10ffcdb232f250d02714ebb98ebfda
Matteo Sanfelici <sanfelicimatteo@gmail.com>
2021-01-02 19:54:30 +0100

    Change probability to 1/2

--- src/environment.dev.ts
+++ src/environment.dev.ts
@@ -1,5 +1,5 @@
 const jwtSecretAdmin = 'secret-univers-ita-jwt-admin';
 const jwtSecretStudents = 'secret-univers-ita-jwt-students';
-const couponMaxCount = 1;
+const couponMaxCount = 2;
 
 export {jwtSecretAdmin, jwtSecretStudents, couponMaxCount}
